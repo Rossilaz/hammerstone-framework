f8bde7aaf0fa9434f62eddccbb693ba02f892ab3
Ross Lazenby <107587925+Rossilaz@users.noreply.github.com>
2022-06-17 19:53:23 +0100

    Update materialUtils.lua

--- scripts/hammerstone/utils/materialUtils.lua
+++ scripts/hammerstone/utils/materialUtils.lua
@@ -12,20 +12,7 @@ function mod:onload(material, edgeDecal)
     function material:setBushMaterialB(baseKey)
       setMaterialB(baseKey, material.types[baseKey].color * 1.1, material.types[baseKey].roughness * 0.8)
     end
-    
-    materialHelper.addMaterial(
-  {
-    id = "leafyRockTEST",
-    baseColor = vec3(0.41,0.4,0.37),
-    baseRoughness = 0.2,
-    baseMetal = 1.0,
-    hairTextureA = edgeDecal.textureLocations.leavesNewA,
-    hairTextureB = edgeDecal.textureLocations.leavesNewB,
-    sizeA = vec2(0.025, 0.1) * 0.8,
-    sizeB = vec2(0.07 * 3.0, 0.125) 
-  }
-)
-  
+      
 end
 
 return mod
